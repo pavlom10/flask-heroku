d87058ea9365ffd795e88d4c04b1606f522fffc8
pavlom <10door@gmail.com>
2021-07-23 19:34:53 +0300

    Linter fixes

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL?sslmode=require')\
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    os.environ.get('DATABASE_URL?sslmode=require')\
     .replace('postgres://', 'postgresql://')
 db = SQLAlchemy(app)
