446005e44212630163db4a07a9f50694d9a26d20
pavlom <10door@gmail.com>
2021-07-23 20:20:39 +0300

    postgres fix

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import os
+import re
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = \
-    os.environ.get('DATABASE_URL?sslmode=require')
+
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+# rest of connection code using the connection string `uri`
+
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 db = SQLAlchemy(app)
