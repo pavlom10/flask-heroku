7922679b48ab2f00327dd65c6039af1ec67f84ff
pavlom <10door@gmail.com>
2021-07-23 19:17:44 +0300

    Test run.py

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
 import os
-from app import app
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'У меня получилось!'
 
 
 if __name__ == '__main__':
@@ -8,3 +16,14 @@ if __name__ == '__main__':
         host='0.0.0.0',
         port=int(os.getenv('PORT', default=5000)),
     )
+
+# import os
+# from app import app
+#
+#
+# if __name__ == '__main__':
+#     app.run(
+#         debug=True,
+#         host='0.0.0.0',
+#         port=int(os.getenv('PORT', default=5000)),
+#     )
